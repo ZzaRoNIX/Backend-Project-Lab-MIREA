6c377d0bf804a9f93a0980f4785f1d0563b18e00
ZzaRoNIX <56475323+ZzaRoNIX@users.noreply.github.com>
2020-10-30 21:34:57 +0300

    Delete views.py

--- Desktop/site_1.1/Project1/Backend/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .models import Matrix
-from .serializer import MatrixSerializer
-
-
-class MatrixCreateView(generics.CreateAPIView):
-    serializer_class = MatrixSerializer
