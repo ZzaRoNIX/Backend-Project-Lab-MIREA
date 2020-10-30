583b4e5c5c7a05bc588439956bc320d07b2a55d8
ZzaRoNIX <56475323+ZzaRoNIX@users.noreply.github.com>
2020-10-30 21:34:45 +0300

    Delete serializer.py

--- Desktop/site_1.1/Project1/Backend/serializer.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Matrix
-
-
-class MatrixSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Matrix
-        fields = ('id', 'A11', 'A12', 'A13', 'A21', 'A22', 'A23', 'A31', 'A32', 'A33')
